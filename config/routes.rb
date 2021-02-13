d41e10978306b0a559efc42ea4c091e9f13d82b9
k.nomo <k.nomoto1225@gmail.com>
2021-02-13 06:28:20 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  # resourcesで必要なアクション指定
+  resources :users, only: [:index, :show, :new, :create]
 end
