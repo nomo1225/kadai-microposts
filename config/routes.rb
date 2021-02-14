41922d176b8dadc851c78c746b0d7ced24cd9a6f
k.nomo <k.nomoto1225@gmail.com>
2021-02-14 06:45:48 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # resourcesで必要なアクション指定
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
